6c492adfbc141ce92781ab4c601603ff3354214e
Vanderlei <vjoliver10@gmail.com>
2025-06-24 20:20:00 -0300

    Adiciona minha primeira página pessoal

---
+++ script.js
@@ -0,0 +1,10 @@
+// Exibe uma saudação no console
+console.log("Olá, Vanderlei! Bem-vindo ao JavaScript.");
+
+// Alerta de boas-vindas
+alert("Essa é sua primeira interação com JavaScript!");
+
+// Altera o conteúdo do título após clicar num botão
+function mudarTitulo() {
+  document.getElementById("titulo").innerText = "Título alterado via JavaScript!";
+}
